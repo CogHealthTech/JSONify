527d5eed1ac48fd88b8b2c317e5358110df58abd
Craig R. Hughes <craig.git@rungie.com>
2014-03-17 15:31:12 -0700

    Initial Commit

---
+++ JSONizer/JSONizer.h
@@ -0,0 +1,13 @@
+//
+//  JSONizer.h
+//  JSONizer
+//
+//  Created by Craig Hughes on 3/17/14.
+//  Copyright (c) 2014 The Cognitive Healthcare Company. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JSONizer : NSObject
+
+@end
---
+++ JSONizer/JSONizer.m
@@ -0,0 +1,13 @@
+//
+//  JSONizer.m
+//  JSONizer
+//
+//  Created by Craig Hughes on 3/17/14.
+//  Copyright (c) 2014 The Cognitive Healthcare Company. All rights reserved.
+//
+
+#import "JSONizer.h"
+
+@implementation JSONizer
+
+@end
